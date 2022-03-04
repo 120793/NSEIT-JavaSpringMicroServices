0c6afe3d6bbe6829d802c2673537953a6f6c5837
Samadhan Sampat Salunke <95615224+120793@users.noreply.github.com>
2022-03-04 17:58:22 +0530

    Add files via upload
    order Serivice

---
+++ orderservice/src/main/java/com/example/orderservice/model/Order.java
@@ -0,0 +1,67 @@
+package com.example.orderservice.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="ordering")
+public class Order {
+	
+	int oid;
+	int cid;
+	String productName;
+	
+	int price;
+
+	public Order() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Order(int oid, int cid, String productName, int price) {
+		super();
+		this.oid = oid;
+		this.cid = cid;
+		this.productName = productName;
+		this.price = price;
+	}
+
+	public int getOid() {
+		return oid;
+	}
+
+	public void setOid(int oid) {
+		this.oid = oid;
+	}
+
+	public int getCid() {
+		return cid;
+	}
+
+	public void setCid(int cid) {
+		this.cid = cid;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+	
+	
+	
+	
+
+}
